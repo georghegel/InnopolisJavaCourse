4448b15b3d5b8b32af19d7b7ca39dfdd7bf3fcb6
georghegel <mirza.bhuseynov@gmail.com>
2024-10-28 19:06:24 +0300

    Ex2 done

---
+++ week3/Ex2/Main.java
@@ -0,0 +1,133 @@
+abstract class Shape {
+    abstract double getArea();
+    abstract double getPerimeter();
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+}
+
+class Square extends Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * side;
+    }
+}
+
+class Triangle extends Shape {
+    private double a, b, c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public double getArea() {
+        double s = (a + b + c) / 2;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    @Override
+    public double getPerimeter() {
+        return a + b + c;
+    }
+}
+
+class Ellipse extends Shape {
+    private double majorAxis;
+    private double minorAxis;
+
+    public Ellipse(double majorAxis, double minorAxis) {
+        this.majorAxis = majorAxis;
+        this.minorAxis = minorAxis;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * majorAxis * minorAxis;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return Math.PI * (3 * (majorAxis + minorAxis) -
+                Math.sqrt((3 * majorAxis + minorAxis) * (majorAxis + 3 * minorAxis)));
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+        Square square = new Square(3.0);
+        Triangle triangle = new Triangle(3.0, 4.0, 5.0);
+        Ellipse ellipse = new Ellipse(5.0, 3.0);
+
+        System.out.println("Circle:");
+        System.out.println("Area: " + circle.getArea());
+        System.out.println("Perimeter: " + circle.getPerimeter());
+
+        System.out.println("\nRectangle:");
+        System.out.println("Area: " + rectangle.getArea());
+        System.out.println("Perimeter: " + rectangle.getPerimeter());
+
+        System.out.println("\nSquare:");
+        System.out.println("Area: " + square.getArea());
+        System.out.println("Perimeter: " + square.getPerimeter());
+
+        System.out.println("\nTriangle:");
+        System.out.println("Area: " + triangle.getArea());
+        System.out.println("Perimeter: " + triangle.getPerimeter());
+
+        System.out.println("\nEllipse:");
+        System.out.println("Area: " + ellipse.getArea());
+        System.out.println("Perimeter (approx): " + ellipse.getPerimeter());
+    }
+}
